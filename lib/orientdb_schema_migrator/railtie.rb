a5eac8e2f929a002486240b84d9c1f67cdc0eb60
Luke Rodgers <lukeasrodgers@gmail.com>
2015-04-01 14:18:23 -0400

    Rails app cannot find rake task, attempting to fix.

--- lib/orientdb_schema_migrator/railtie.rb
+++ lib/orientdb_schema_migrator/railtie.rb
@@ -4,7 +4,7 @@ require 'rails'
 module OrientdbSchemaMigrator
   class Railtie < Rails::Railtie
     rake_tasks do
-      require 'tasks/orientdb_schema_migrator.rake'
+      load 'tasks/orientdb_schema_migrator.rake'
     end
   end
 end
